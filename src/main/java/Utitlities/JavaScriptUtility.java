4b6e76507e086229c35b7e552846712f4cc692d2
Evez Aslanov <evez.aslanov.94@gmail.com>
2025-06-04 23:45:51 +0400

    agencies about page load but stil have problems

--- src/main/java/Utitlities/JavaScriptUtility.java
+++ src/main/java/Utitlities/JavaScriptUtility.java
@@ -40,40 +40,91 @@ public class JavaScriptUtility {
     }
 
     // Elementə scroll etmək üçün təkmilləşdirilmiş metod
+//    public static void scrollToElementJS(By locator) {
+//        WebElement element = driver.findElement(locator);
+//        String scrollScript = "arguments[0].scrollIntoView({"
+//                + "behavior: 'smooth', "
+//                + "block: 'center', "
+//                + "inline: 'center'"
+//                + "});";
+//        ((JavascriptExecutor) driver).executeScript(scrollScript, element);
+//        wait(300); // Kiçik gözləmə
+//    }
+//
+//    // Təhlükəsiz klik üçün metod
+//    public static void safeClick(By locator) {
+//        WebElement element = driver.findElement(locator);
+//        scrollToElementJS(locator);
+//
+//        try {
+//            // 1. Normal klik
+//            element.click();
+//        } catch (Exception e1) {
+//            try {
+//                // 2. Action zənciri ilə klik
+//                new Actions(driver)
+//                        .moveToElement(element)
+//                        .pause(Duration.ofMillis(300))
+//                        .click()
+//                        .perform();
+//            } catch (Exception e2) {
+//                // 3. JavaScript ilə klik
+//                clickWithJS(element);
+//            }
+//        }
+//    }
     public static void scrollToElementJS(By locator) {
-        WebElement element = driver.findElement(locator);
-        String scrollScript = "arguments[0].scrollIntoView({"
-                + "behavior: 'smooth', "
-                + "block: 'center', "
-                + "inline: 'center'"
-                + "});";
-        ((JavascriptExecutor) driver).executeScript(scrollScript, element);
-        wait(300); // Kiçik gözləmə
-    }
-
-    // Təhlükəsiz klik üçün metod
-    public static void safeClick(By locator) {
-        WebElement element = driver.findElement(locator);
-        scrollToElementJS(locator);
-
         try {
-            // 1. Normal klik
-            element.click();
-        } catch (Exception e1) {
-            try {
-                // 2. Action zənciri ilə klik
-                new Actions(driver)
-                        .moveToElement(element)
-                        .pause(Duration.ofMillis(300))
-                        .click()
-                        .perform();
-            } catch (Exception e2) {
-                // 3. JavaScript ilə klik
-                clickWithJS(element);
-            }
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+            WebElement element = wait.until(ExpectedConditions.presenceOfElementLocated(locator));
+
+            String scrollScript = "arguments[0].scrollIntoView({"
+                    + "behavior: 'smooth', "
+                    + "block: 'center', "
+                    + "inline: 'center'"
+                    + "});";
+            ((JavascriptExecutor) driver).executeScript(scrollScript, element);
+            wait(300);
+        } catch (Exception e) {
+            throw new RuntimeException("Element scroll edilərkən xəta baş verdi: " + locator, e);
         }
     }
 
+    public static void safeClick(By locator) {
+        try {
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+            WebElement element = wait.until(ExpectedConditions.elementToBeClickable(locator));
+
+            // Elementin görünənə qədər scroll et
+            ((JavascriptExecutor) driver).executeScript(
+                    "arguments[0].scrollIntoView({block: 'center', behavior: 'smooth'});",
+                    element
+            );
+
+            // 3 cür klik cəhdi
+            try {
+                element.click();
+            } catch (Exception e1) {
+                try {
+                    new Actions(driver)
+                            .moveToElement(element)
+                            .pause(Duration.ofMillis(500))
+                            .click()
+                            .perform();
+                } catch (Exception e2) {
+                    ((JavascriptExecutor) driver).executeScript(
+                            "arguments[0].click();",
+                            element
+                    );
+                }
+            }
+
+            // Kiçik gözləmə əlavə et
+            Thread.sleep(300);
+        } catch (Exception e) {
+            throw new RuntimeException("Element klik edilərkən xəta: " + locator, e);
+        }
+    }
     // JavaScript ilə klik üçün metod
     public static void clickWithJS(WebElement element) {
         ((JavascriptExecutor) driver).executeScript("arguments[0].click();", element);
--- src/main/java/com/e_services_economy/pages/NavbarComponents/AgenciesPage.java
+++ src/main/java/com/e_services_economy/pages/NavbarComponents/AgenciesPage.java
@@ -3,6 +3,7 @@ package com.e_services_economy.pages.NavbarComponents;
 import Utitlities.JavaScriptUtility;
 import com.e_services_economy.pages.HomePage;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -18,7 +19,7 @@ public class AgenciesPage extends HomePage {
     private By agenciesHeaderText = By.xpath("//div[@id='root']//h2[text()='Qurumlar']");
     private By searchInput = By.xpath("//div[@id='root']//input[@name='text']");
     private By resultItems = By.xpath("//div[@id='root']//span[@class='_title_5ekyk_45']");
-    private By aboutAgenciesLink = By.xpath("(//div[@id='root']//div[@class='_corps_5ekyk_35']//div[@class='_corp_5ekyk_35']//a[text()='Qurum məlumatları'])[1]");
+    private By aboutAgenciesLink = By.xpath("(//div[@id='root']//div[@class='_corps_5ekyk_35']//div[@class='_corp_5ekyk_35']//a)[1]");
     private By aboutAgenciesTitle = By.xpath("//div[@id='root']//div[@class='_top_5ekyk_42']//span[1]");
 
 
@@ -55,15 +56,71 @@ public class AgenciesPage extends HomePage {
 
     // About Agencies
 
-    public  AboutAgenciesPage goToAboutAgenciesPage(){
-        // JavaScriptUtility istifadəsi
-        JavaScriptUtility.setDriver(driver);
-        JavaScriptUtility.scrollToElementJS(aboutAgenciesLink);
-        JavaScriptUtility.safeClick(aboutAgenciesLink);
-        JavaScriptUtility.waitForPageLoad(10);
+//    public  AboutAgenciesPage goToAboutAgenciesPage(){
+//        // JavaScriptUtility istifadəsi
+//        JavaScriptUtility.setDriver(driver);
+//        JavaScriptUtility.scrollToElementJS(aboutAgenciesLink);
+//        JavaScriptUtility.safeClick(aboutAgenciesLink);
+//        JavaScriptUtility.waitForPageLoad(10);
+//
+//        return new AboutAgenciesPage();
+//
+//
 
-        return new AboutAgenciesPage();
 
+    public AboutAgenciesPage goToAboutAgenciesPage() {
+        try {
+            // 1. Elementin görünənə qədər gözlə
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+            WebElement link = wait.until(ExpectedConditions.visibilityOfElementLocated(aboutAgenciesLink));
+
+            // 2. JavaScript ilə scroll et və klik et
+            JavaScriptUtility.scrollToElementJS(aboutAgenciesLink);
+            JavaScriptUtility.safeClick(aboutAgenciesLink);
+
+            // 3. Yeni kontentin yüklənməsini gözlə
+            waitForPageContentLoad();
+
+            // 4. URL dəyişikliyini yoxla (əgər varsa)
+            if (!isUrlChanged()) {
+                throw new RuntimeException("Səhifə keçidi baş vermədi");
+            }
+
+            return new AboutAgenciesPage();
+        } catch (Exception e) {
+//            takeScreensho("goToAboutAgenciesPage_error");
+            throw new RuntimeException("'Qurum məlumatları' səhifəsinə keçid edilərkən xəta: " + e.getMessage());
+        }
+    }
+
+    private void waitForPageContentLoad() {
+        try {
+            // Xüsusi elementin görünməsini gözlə
+            new WebDriverWait(driver, Duration.ofSeconds(15))
+                    .until(ExpectedConditions.visibilityOfElementLocated(
+                            By.xpath("//div[contains(@class, 'text-xl font-[500]')]")));
+
+            // Və ya JavaScript ilə səhifə hazırlığını yoxla
+            new WebDriverWait(driver, Duration.ofSeconds(15))
+                    .until(d -> ((JavascriptExecutor) d)
+                            .executeScript("return document.readyState").equals("complete"));
+        } catch (Exception e) {
+//            takeScreenshot("waitForPageContentLoad_error");
+            throw e;
+        }
+    }
+
+    private boolean isUrlChanged() {
+        // Əvvəlki URL-i saxla
+        String currentUrl = driver.getCurrentUrl();
+
+        // Kiçik gözləmə
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException ignored) {}
+
+        // URL dəyişibsə true qaytar
+        return !currentUrl.equals(driver.getCurrentUrl());
     }
 
     public String getAgencyTitleText(){
